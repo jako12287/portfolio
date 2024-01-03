37dc58b11d77d6c349a413d46d5884ee1d4c3ee1
Johan Cortes <jako12287@gmail.com>
2024-01-03 18:27:53 -0500

    Menu mockup

--- src/components/sidebarMenu/index.tsx
+++ src/components/sidebarMenu/index.tsx
@@ -1,8 +1,52 @@
 import { SidebarMenuProps } from "@/types";
 import styles from "./sidebarMenu.module.css";
+import { IoClose } from "react-icons/io5";
+import { FiClipboard, FiMonitor, FiPhoneCall, FiThumbsUp } from "react-icons/fi";
+import { FiCodesandbox } from "react-icons/fi";
+import { GoBook } from "react-icons/go";
+
+const items = [
+    {
+        id: "001",
+        icon: <FiClipboard size={40} color="#f17d3e" />,
+        title: "Acerca de mi"
+    },
+    {
+        id: "002",
+        icon: <FiCodesandbox size={40} color="#f17d3e" />,
+        title: "Proyectos"
+    },
+    {
+        id: "003",
+        icon: <FiMonitor size={40} color="#f17d3e" />,
+        title: "Habilidades"
+    },
+    {
+        id: "004",
+        icon: <GoBook size={40} color="#f17d3e" />,
+        title: "Estudios"
+    },
+    {
+        id: "005",
+        icon: <FiPhoneCall size={40} color="#f17d3e" />,
+        title: "Contacto"
+    }
+]
 const SidebarMenu = ({ open, toggleSidebar }: SidebarMenuProps) => {
-    return <div className={styles.container}>sidebar menu
-        <h1 onClick={toggleSidebar}>X</h1></div>;
+    return <div className={styles.container}>
+        <div className={styles.headerMenu}>
+            <div className={styles.textName}>
+                Johan Cortes
+            </div>
+            <IoClose size="40" onClick={toggleSidebar} />
+        </div>
+        <div className={styles.bodyMenu}>
+            {items?.map(({ id, title, icon }) => <div key={id} className={styles.itemMenu}>
+                {icon}
+                {title}
+            </div>)}
+        </div>
+    </div>;
 };
 
 export default SidebarMenu;
